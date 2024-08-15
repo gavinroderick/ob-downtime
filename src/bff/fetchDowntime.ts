44b2ba8b927a6b4e20d6460f86c670e1b5a8a2a8
Gavin Roderick <gavin.roderick26@gmail.com>
2024-08-16 00:12:19 +0100

    hack: deal with a weird ticket number bug

--- src/bff/fetchDowntime.ts
+++ src/bff/fetchDowntime.ts
@@ -10,16 +10,23 @@ export async function fetchDowntime(): Promise<Downtime[]> {
 function mapRawToDowntime(raw: object): Downtime[] {
   const downtimes: Downtime[] = [];
 
-  for (const [key, value] of Object.entries(raw)) {
+  for (let [key, value] of Object.entries(raw)) {
+    // hack for stupid ticket number bug that I can't figure out
+    let index = 0;
+    if (key === "OBIE-1") {
+      key = "OBIE-12492";
+      index = 1;
+    }
+
     const downtime: Downtime = {
       ticketId: key,
-      status: value[0],
-      provider: value[1],
-      startDate: value[2],
-      endDate: value[3],
-      createdDate: value[4],
-      affectedApis: value[5].split("/"),
-      description: value.slice(6).join(" "),
+      status: value[index],
+      provider: value[index + 1],
+      startDate: value[index + 2],
+      endDate: value[index + 3],
+      createdDate: value[index + 4],
+      affectedApis: value[index + 5].split("/"),
+      description: value.slice(index + 6).join(" "),
       link: `https://openbanking.atlassian.net/browse/${key}`,
     };
     downtimes.push(downtime);
